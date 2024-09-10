3b9038918bbc7e8c6429466cef238420a0a2dd56
Charlie Crosley <charlie1crosley@gmail.com>
2024-09-10 17:33:58 +0100

    Cleaned gh-pages branch and added build files

--- src/utils/api_constants.ts
+++
@@ -1,8 +0,0 @@
-
-export const options_get = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNmJlM2MyOTI2NjA5NTRhM2Y4OGRiNjMyZWU0ZWQ5YiIsIm5iZiI6MTcyNTgwNjYxNS44OTQxNywic3ViIjoiNjZkZGI2MGJkYTcwNjU0M2Y1M2FiOTJjIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.cFw9HN1DiIGZJ-34CaQmcJG8TAgTE4k6vxKPP3so168'
-    }
-};
